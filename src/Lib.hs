9a43ed2a00d435c49ec5922ba6b90c758a5979a2
Ingun Jon <ingun37@gmail.com>
2022-03-16 21:27:54 +0900

    use pandoc

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,37 +1,50 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib (parse) where
 
 import Crypto.Hash.SHA1 as SHA (hash)
 import Data.Aeson (ToJSON)
-import qualified Data.Aeson as J
+import Data.Aeson as J (encode)
 import Data.Bifunctor (bimap)
-import qualified Data.ByteString.Base16 as B16
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.UTF8 as BzUTF8
+import Data.ByteString.Base16 as B16 (encode)
+import qualified Data.ByteString.Char8 as C8 (pack, unpack)
+import Data.ByteString.Lazy.UTF8 (toString)
 import Data.Map (Map, fromList)
-import qualified Data.Tree as T
+import qualified Data.Text as Txt (Text, pack, unpack)
+import Data.Tree (Tree (Node), flatten, rootLabel)
 import GHC.Generics (Generic)
-import qualified System.Directory as D
+import System.Directory (copyFile, createDirectoryIfMissing, removePathForcibly)
 import System.Directory.Tree
   ( DirTree (Dir, File),
     filterDir,
     readDirectoryWithL,
     zipPaths,
   )
-import System.FilePath.Posix (takeBaseName, takeFileName, (</>))
-import qualified System.FilePath.Posix as P
+import System.FilePath.Posix (dropFileName, isExtensionOf, makeRelative, takeBaseName, takeFileName, (-<.>), (</>))
+import Text.Pandoc
+  ( Extension (Ext_tex_math_dollars, Ext_tex_math_double_backslash),
+    HTMLMathMethod (KaTeX),
+    def,
+    defaultKaTeXURL,
+    extensionsFromList,
+    readMarkdown,
+    readerExtensions,
+    runIOorExplode,
+    writeHtml5String,
+    writerHTMLMathMethod,
+  )
 import Text.Regex.PCRE.Heavy (gsub, scan)
-import qualified Util as U
+import Text.Regex.PCRE.Light (compile, multiline, dotall)
 
 data Item = Item
   { title :: String,
     sha1 :: String,
-    attr :: (Map String String)
+    attr :: Map String String
   }
   deriving (Generic, Show)
 
-data Node = Node
+data TemTree = TemTree
   { path :: String,
     item :: Item,
     kids :: [Item],
@@ -39,51 +52,79 @@ data Node = Node
   }
   deriving (Generic, Show)
 
-sha1InHex = B.unpack . B16.encode . SHA.hash . B.pack
+sha1InHex = C8.unpack . B16.encode . SHA.hash . C8.pack
 
+-- Tree, rootLabel, Node, flatten
 --it generates tree from bottom-up (dynamic programming)
-makeTr :: String -> String -> [DirTree String] -> T.Tree Node
+makeTr :: String -> String -> [DirTree String] -> Tree TemTree
 makeTr path parentSha1 entries =
   let sha1 = sha1InHex path
       kidTrs = [makeTr (path </> title) sha1 entries' | Dir title entries' <- entries]
-      kidItems = Prelude.map (item . T.rootLabel) kidTrs
+      kidItems = Prelude.map (item . rootLabel) kidTrs
       thisItem = Item (takeFileName path) sha1 (fromList [(takeBaseName name', file) | File name' file <- entries])
-      thisNode = Node path thisItem kidItems parentSha1
-   in T.Node thisNode kidTrs
+      thisNode = TemTree path thisItem kidItems parentSha1
+   in Node thisNode kidTrs
 
 instance ToJSON Item
 
-instance ToJSON Node
+instance ToJSON TemTree
 
-writeJson' :: String -> Node -> IO ()
+writeJson' :: String -> TemTree -> IO ()
 writeJson' dst =
-  let f = (dst </>) . (P.-<.> ".json") . sha1 . item -- make destination file path from Node
-      g = BzUTF8.toString . J.encode -- make json from Node
-   in (uncurry writeFile) . bimap f g . (\x -> (x, x)) -- dup :: a -> (a,a)
+  let f = (dst </>) . (-<.> ".json") . sha1 . item -- make destination file path from Node
+      g = toString . J.encode -- make json from Node
+   in uncurry writeFile . bimap f g . (\x -> (x, x)) -- dup :: a -> (a,a)
 
 dirFilter :: DirTree a -> Bool
-dirFilter (File name _) = ".md" `P.isExtensionOf` name
+dirFilter (File name _) = ".md" `isExtensionOf` name
 dirFilter _ = True
 
 parse :: FilePath -> FilePath -> IO ()
 parse src dst = do
-  mdDir <- (filterDir dirFilter) . zipPaths <$> readDirectoryWithL readFile src
+  mdDir <- filterDir dirFilter . zipPaths <$> readDirectoryWithL readFile src
   let imgDst = dst </> "imgs"
   let dbDst = dst </> "db"
-  let f = \x -> D.removePathForcibly x >> D.createDirectoryIfMissing True x -- clean the directory by removing and then making again
+  let f = \x -> removePathForcibly x >> createDirectoryIfMissing True x -- clean the directory by removing and then making again
   mapM_ f [imgDst, dbDst]
   mdDir' <- traverse (mdTraverse imgDst dst) mdDir
   let (Dir name entries) = mdDir'
-  mapM_ (writeJson' dbDst) (T.flatten (makeTr name "" entries))
+  mapM_ (writeJson' dbDst) (flatten (makeTr name "" entries))
+
+mdToHTML :: Txt.Text -> IO Txt.Text
+mdToHTML txt =
+  runIOorExplode $
+    readMarkdown
+      def
+        { readerExtensions = extensionsFromList [Ext_tex_math_double_backslash]
+        }
+      txt
+      >>= writeHtml5String
+        def
+          { writerHTMLMathMethod = KaTeX defaultKaTeXURL
+          }
 
 -- change ![](a/b/c.jpg) to ![](assets/{$1}/c.jpg)
 -- copy all the images to assets/$1/
 mdTraverse :: FilePath -> FilePath -> (FilePath, String) -> IO String
 mdTraverse assetsPath relTo (path, content) = do
-  let mdDir = P.dropFileName path
-  let matches = map (head . snd) $ scan U.imgRegex content
-  mapM_ (D.createDirectoryIfMissing True . (assetsPath </>) . P.dropFileName) matches
-  mapM_ (\m -> D.copyFile (mdDir </> m) (assetsPath </> m)) matches
-  let rel = P.makeRelative relTo assetsPath
-  return $ gsub U.imgRegex (\(d:_) -> "![](assets/" ++ rel ++ "/" ++ d :: String)  content
-  -- return $ gsub U.imgRegex (U.compileRegex ("![](assets/" ++ rel ++ "/\\1)")) content
+  let imgRegex = compile "!\\[\\]\\((?!http)(.+?)\\)" []
+  let mdDir = dropFileName path
+  let matches = map (head . snd) $ scan imgRegex content
+  mapM_ (createDirectoryIfMissing True . (assetsPath </>) . dropFileName) matches
+  mapM_ (\m -> copyFile (mdDir </> m) (assetsPath </> m)) matches
+  let rel = makeRelative relTo assetsPath
+  let md = subDisplayMathBlock $ subInlineMathBlock $ gsub imgRegex (\(d : _) -> "![](/" ++ rel ++ "/" ++ d ++ ")" :: String) content
+  html <- mdToHTML (Txt.pack md)
+  return $ Txt.unpack html
+
+subInlineMathBlock :: String -> String
+subInlineMathBlock =
+  let imgRegex = compile "\\$`(.+?)`\\$" []
+   in gsub imgRegex (\(d : _) -> "\\\\(" ++ d ++ "\\\\)" :: String)
+
+subDisplayMathBlock :: String -> String
+subDisplayMathBlock =
+  let imgRegex = compile "^```math$(.+?)^```$" [multiline, dotall]
+   in gsub imgRegex (\(d : _) -> "\\\\[" ++ d ++ "\\\\]" :: String)
+
+-- copyFile, createDirectoryIfMissing, removePathForcibly, createDirectoryIfMissing
