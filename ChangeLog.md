a12410eb4714bafe45dc6cfb088e8e325de1bbd3
Ingun Jon <ingun37@gmail.com>
2023-04-18 11:11:05 +0900

    cabal

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,5 @@
-# Changelog for answers-script
+# Revision history for answers-script
 
-## Unreleased changes
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ shell.nix
@@ -0,0 +1,38 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, aeson-pretty, base, base16-bytestring
+      , bytestring, containers, cryptohash-sha1, directory
+      , directory-tree, filepath, hlibgit2, lens, lib
+      , optparse-applicative, pandoc, pcre-heavy, pcre-light, text
+      }:
+      mkDerivation {
+        pname = "answers-script";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          aeson aeson-pretty base base16-bytestring bytestring containers
+          cryptohash-sha1 directory directory-tree filepath hlibgit2 lens
+          pandoc pcre-heavy pcre-light text
+        ];
+        executableHaskellDepends = [ base optparse-applicative ];
+        license = "unknown";
+        mainProgram = "answers-script";
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
