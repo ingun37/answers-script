8fd6ba8b1bf59a8e55a4eb929a3249a70269ad77
Ingun Jon <ingun37@gmail.com>
2025-09-06 23:24:06 +0900

    reading directory and very basic test data

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,17 @@
 module MyLib (someFunc) where
+import qualified System.Directory.Tree as Dir
+import qualified System.FilePath as File
+import Control.Lens
 
 someFunc :: String -> FilePath -> FilePath -> IO ()
-someFunc prefixPath src dst = putStrLn $ "someFunc" ++ prefixPath ++ src ++ dst
+someFunc prefixPath src dst = do
+    root' <- Dir.build src
+    let root = over Dir._dirTree (Dir.filterDir theFilter) root'
+    print root
+
+theFilter :: Dir.DirTree a -> Bool
+theFilter =
+    \case
+        Dir.Dir name _ -> head name /= '.'
+        Dir.File name _ -> ".md" `File.isExtensionOf` name || ".txt" `File.isExtensionOf` name
+        _ -> False
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,17 @@
 module Main (main) where
 
+import qualified MyLib
+
+src :: FilePath
+src = "test/src"
+
+dst :: FilePath
+dst = "test/dst"
+
+prefix :: String
+prefix = "prefix"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+    MyLib.someFunc prefix src dst
+    putStrLn "Test suite not yet implemented."
