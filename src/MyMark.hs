7b3dc00fb98e2c4806fa5ca770e25e21eba6a631
ingun <ingun37@gmail.com>
2025-09-11 13:11:33 +0900

    distinguish code and math

--- src/MyMark.hs
+++ src/MyMark.hs
@@ -9,6 +9,24 @@ prefixImageUrl prefix node =
       replaceUrl url = if T.isPrefixOf (T.pack "http") url then url else safePrefix <> T.dropWhile (== '/') url
       recurse (Node posInfo nodeType nodes) =
         case nodeType of
-          IMAGE url title -> Node posInfo (IMAGE (replaceUrl url) title) nodes
-          _ -> Node posInfo nodeType (recurse <$> nodes)
+          IMAGE url title -> Node Nothing (IMAGE (replaceUrl url) title) nodes
+          PARAGRAPH -> Node Nothing PARAGRAPH $ workOnInlineMath nodes
+          CODE_BLOCK info text -> if info == T.pack "math" then mathBlock text else Node Nothing nodeType nodes
+          _ -> Node Nothing nodeType (recurse <$> nodes)
    in recurse node
+
+workOnInlineMath :: [Node] -> [Node]
+workOnInlineMath (x : y : z : tail) =
+  case (x, y, z) of
+    (Node _ (TEXT l) [], Node _ (CODE m) [], Node _ (TEXT r) []) ->
+      if T.isSuffixOf (T.pack "$") l && T.isPrefixOf (T.pack "$") r
+        then x : Node Nothing (TEXT (T.pack "`" <> m <> T.pack "`")) [] : workOnInlineMath (z : tail)
+        else x : y : workOnInlineMath (z : tail)
+    _ -> x : workOnInlineMath (y : z : tail)
+workOnInlineMath (x : xs) = x : workOnInlineMath xs
+workOnInlineMath [] = []
+
+mathBlock :: T.Text -> Node
+mathBlock text =
+  let t = T.pack "\\\\(" <> text <> T.pack ")\\\\"
+   in Node Nothing PARAGRAPH [Node Nothing (TEXT t) []]
