fa9177cc4acb2c7579ef310d7e832c8c6e80f4ba
ingun <ingun37@gmail.com>
2025-09-14 17:51:34 +0900

    add subcommands

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,15 @@ module Main where
 import MyLib (someFunc)
 import Options.Applicative
 
-data Sample = Sample
+data BuildAssetArgs = BuildAssetArgs
   { prefix :: String,
     src :: String,
     dst :: String
   }
 
-sample :: Parser Sample
-sample =
-  Sample
+buildAssetParser :: Parser BuildAssetArgs
+buildAssetParser =
+  BuildAssetArgs
     <$> strOption
       ( long "prefix"
           <> value ""
@@ -26,18 +26,64 @@ sample =
           <> help "path to static directory"
       )
 
-greet :: Sample -> IO ()
-greet (Sample prefix src dst) = do
+buildAssetCommand :: ParserInfo BuildAssetArgs
+buildAssetCommand =
+  info
+    (buildAssetParser <**> helper)
+    ( fullDesc
+        <> progDesc "Import answers-db into answers static asset"
+        <> header "Import answers-db into answers static asset"
+    )
+
+data SplitMatlabMarkdownArgs = SplitMatlabMarkdownArgs FilePath FilePath
+
+splitMatlabMarkdownParser :: Parser SplitMatlabMarkdownArgs
+splitMatlabMarkdownParser =
+  SplitMatlabMarkdownArgs
+    <$> strOption
+      ( long "src"
+          <> help "path to the markdown file that is exported from MATLAB"
+      )
+    <*> strOption
+      ( long "dst"
+          <> help "path to a directory"
+      )
+
+splitMatlabMarkdownCommand :: ParserInfo SplitMatlabMarkdownArgs
+splitMatlabMarkdownCommand =
+  info
+    (splitMatlabMarkdownParser <**> helper)
+    ( fullDesc
+        <> progDesc "Build assets from a markdown file exported from MATLAB"
+        <> header "Build assets from a markdown file exported from MATLAB"
+    )
+
+data Args = BuildAssets BuildAssetArgs | SplitMatlabMarkdown SplitMatlabMarkdownArgs
+
+-- data Args = Args { commandArgs :: CommandArgs }
+
+argsParser :: Parser Args
+argsParser =
+  hsubparser
+    ( command "build-assets" (BuildAssets <$> buildAssetCommand)
+        <> command "split-matlab-markdown" (SplitMatlabMarkdown <$> splitMatlabMarkdownCommand)
+    )
+
+greet :: Args -> IO ()
+greet (BuildAssets (BuildAssetArgs prefix src dst)) = do
   _ <- someFunc prefix src dst
+  return ()
+greet (SplitMatlabMarkdown (SplitMatlabMarkdownArgs src dst)) = do
   return ()
 
 main :: IO ()
-main = greet =<< execParser opts
-  where
-    opts =
-      info
-        (sample <**> helper)
-        ( fullDesc
-            <> progDesc "import answers-db into answers static asset"
-            <> header "what is header?"
-        )
+main =
+  greet
+    =<< execParser
+      ( info
+          (argsParser <**> helper)
+          ( fullDesc
+              <> progDesc "Collection of utility functions for the Answers web app"
+              <> header "Collection of utility functions for the Answers web app"
+          )
+      )
